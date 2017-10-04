0da6d1d752d12047f6a7e01ed63199cd56a58060
Kevin Singh <singhk7@yahoo.com>
2017-10-03 21:30:22 -0500

    basic session implemented, jsoup dependency added

--- src/OpenStackMeetingsServlet.java
+++ src/OpenStackMeetingsServlet.java
@@ -1,4 +1,11 @@
-
+/* 
+ * Author: Kevin Singh
+ *   Date: 09/26/2018
+ *   
+ * A basic servlet application to handle sessions, query parameters and 
+ *   	parsing using jsoup
+ *   
+ */
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -6,6 +13,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class OpenStackMeetingsServlet extends HttpServlet {
 	
@@ -16,11 +27,49 @@ public class OpenStackMeetingsServlet extends HttpServlet {
 		String session = request.getParameter("session");
 		boolean startSession = false;
 		if(session != null) {
-			startSession = session.equals("start");
+			startSession = session.equalsIgnoreCase("start");
 		}
+		
+		if(startSession) {
+			Cookie cookies[] = request.getCookies();
+            boolean activeSession = false;
+            for(int i=0; cookies != null && i<cookies.length; i++) {
+                    Cookie ck = cookies[i];
+                    String cookieName = ck.getName();
+                    String cookieValue = ck.getValue();
+                    if ((cookieName != null && cookieName.equals("active-session")) 
+                                    && cookieValue != null && cookieValue.equals("true")) {
+                            response.getWriter().println("Session Active. Welcome! :)");
+                            activeSession = true;
+                    }
+            }
+            if (!activeSession) {
+                    Cookie cookie = new Cookie("active-session","true");
+                    cookie.setDomain("localhost");
+                    cookie.setPath("/assignment1" + request.getServletPath());
+                    cookie.setHttpOnly(true);
+                    cookie.setMaxAge(60);
+                    response.addCookie(cookie);
+                    response.getWriter().println("No active session");
+            }
+			
+		}
+		
+		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+		Elements newsHeadlines = doc.select("#mp-itn b a");
+		
 		response.getWriter().println("Start session " + startSession);
 		
 		response.getWriter().println("OpenStackMeetingsServlet");
     }
 
 }
+
+
+
+
+
+
+
+
+
